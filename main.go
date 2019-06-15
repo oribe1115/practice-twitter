1c5e1cf0d5066adebb706dc8144632a7c171995d
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-06-15 19:46:55 +0900

    echo周りを書いた

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
 	"github.com/oribe1115/practice-twitter/model"
 )
 
 func main() {
 	model.LoadEnv()
 	model.GetTwitterAPI()
+
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:8080"},
+		AllowCredentials: true,
+	}))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+
+	e.Start(":" + port)
 
 }
